97e85b19fa8f0ca07f667d12b04ddf6b70b70580
idisfkj <idisfkj@gmail.com>
2020-02-13 10:53:31 +0800

    init project

---
+++ android/app/src/main/kotlin/com/idisfkj/flutter_github/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.idisfkj.flutter_github
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
