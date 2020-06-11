3c0112b8998954c62aae2a5eab46e0440255bb6d
idisfkj <idisfkj@gmail.com>
2020-06-11 16:36:38 +0800

    feat: 删除test代码

--- lib/ui/home/notification/notification.dart
+++ lib/ui/home/notification/notification.dart
@@ -4,8 +4,6 @@ import 'package:flutter_github/model/notification_model.dart';
 import 'package:flutter_github/ui/base/base_page.dart';
 import 'package:flutter_github/ui/base/base_state.dart';
 import 'package:flutter_github/ui/home/notification/notification_change_model.dart';
-import 'package:flutter_github/ui/home/notification/test_gesture_detector.dart';
-import 'package:flutter_github/ui/home/notification/test_text.dart';
 import 'package:provider/provider.dart';
 
 import 'notification_vm.dart';
@@ -34,30 +32,40 @@ class _NotificationPageState
             return ChangeNotifierProvider<NotificationChangeModel>(
               create: (context) => NotificationChangeModel(item.unread),
               child: Consumer<NotificationChangeModel>(
-                builder: (context, item, child) => TestGestureDetector(
-                  onTap: () {
-                    vm.contentTap(index, context);
+                builder: (context, item, child) => Builder(
+                  builder: (context) {
+                    print("GestureDetector build");
+                    return GestureDetector(
+                      onTap: () {
+                        vm.contentTap(index, context);
+                      },
+                      child: Container(
+                          color: item.unread
+                              ? Colors.white
+                              : Color.fromARGB(13, 0, 0, 0),
+                          padding: EdgeInsets.only(
+                              left: 15.0, top: 10.0, right: 15.0),
+                          child: child),
+                    );
                   },
-                  child: Container(
-                      color: item.unread
-                          ? Colors.white
-                          : Color.fromARGB(13, 0, 0, 0),
-                      padding:
-                          EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0),
-                      child: child),
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    TestText(
-                      item.repository.fullName,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                        color: item.unread
-                            ? Colors.black87
-                            : Color.fromARGB(255, 102, 102, 102),
-                      ),
+                    Builder(
+                      builder: (context) {
+                        print("Text build");
+                        return Text(
+                          item.repository.fullName,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                            color: item.unread
+                                ? Colors.black87
+                                : Color.fromARGB(255, 102, 102, 102),
+                          ),
+                        );
+                      },
                     ),
                     Row(
                       children: <Widget>[
